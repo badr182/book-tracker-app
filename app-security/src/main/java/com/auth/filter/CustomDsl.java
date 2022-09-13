6fc654f82aee12fc33e8cfa39f90ce9503c6dbef
badrakkar182@gmail.com <badrakkar182@gmail.com>
2022-09-13 14:14:36 +0100

    update CustomDsl

--- app-security/src/main/java/com/auth/filter/CustomDsl.java
+++ app-security/src/main/java/com/auth/filter/CustomDsl.java
@@ -1,19 +1,20 @@
 package com.auth.filter;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 
 
 public class CustomDsl extends AbstractHttpConfigurer<CustomDsl, HttpSecurity>{
 
-	
     @Override
     public void configure(HttpSecurity http) throws Exception {
         AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
 		CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManager);
-		customAuthenticationFilter.setFilterProcessesUrl("/api/login");
+		customAuthenticationFilter.setFilterProcessesUrl("/login");
 		http.addFilter(new CustomAuthenticationFilter(authenticationManager));
     }
 
