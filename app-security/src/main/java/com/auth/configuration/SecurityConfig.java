779459acbc9226154e6216a11a7d39c9500fcddb
badrakkar182@gmail.com <badrakkar182@gmail.com>
2022-09-15 22:07:51 +0100

    update SecurityConfig

--- app-security/src/main/java/com/auth/configuration/SecurityConfig.java
+++ app-security/src/main/java/com/auth/configuration/SecurityConfig.java
@@ -1,39 +1,55 @@
 package com.auth.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.auth.filter.CustomAuthenticationFilter;
+import com.auth.filter.CustomAuthorizationFilter;
 import com.auth.filter.CustomDsl;
+import com.auth.services.UserServiceImpl;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
+@Slf4j
 public class SecurityConfig {
-	
-	
+		
 	private final UserDetailsService userDetailsService;
 
 	private final BCryptPasswordEncoder bCryptPasswordEncoder; 
 	
-    public DaoAuthenticationProvider authenticationProvider() {
-    	
+	//@Bean
+    public DaoAuthenticationProvider authenticationProvider() {    	
         DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
         auth.setUserDetailsService(userDetailsService);
         auth.setPasswordEncoder(bCryptPasswordEncoder);
         
         return auth;
     }
-	
+    	
+   @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+  
 	
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
@@ -41,13 +57,14 @@ public class SecurityConfig {
 		http.csrf().disable();
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		http.authorizeHttpRequests( (authz) -> authz
-				.antMatchers("/api/login","/api/root","/api/user/register").permitAll()
-				.antMatchers(HttpMethod.POST, "/api/login").permitAll()
-				.anyRequest().authenticated()				
+				.antMatchers("/login","/api/root","/api/user/register","/error").permitAll()
+				.antMatchers(HttpMethod.POST, "/login").permitAll()
+				.anyRequest().authenticated()
 				);
-
-		//http.authenticationProvider(authenticationProvider());		
+		//http.authenticationManager(authenticationManager);		
+		// antMatchers("/error").permitAll()
 		http.apply(CustomDsl.getCustomDsl());
+		http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
 		http.cors();
 		return http.build();
 	}
